1058cc4d48ae22a946c11a5bdb9fe62c9b3d2a8f
DyerSylvie <sylvierui1@gmail.com>
2024-07-08 23:08:15 -0700

    window skeleton

--- script.js
+++ script.js
@@ -75,3 +75,22 @@ function subFilter(e, label) {
     e.currentTarget.className += " active";
 }
 
+// For Dropdown
+function dropdownOptions() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menuItem')) {
+    var dropdowns = document.getElementsByClassName("menuItems");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
